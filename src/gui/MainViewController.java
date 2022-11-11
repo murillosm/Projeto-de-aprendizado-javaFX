3188cf8e48acce22d11f7c2f8ced06ae5ae54967
murillosm <murillosm@hotmail.com>
2022-11-11 18:31:37 -0300

    adicionando o acesso ao banco de dados

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,10 +1,9 @@
 package gui;
 
-
 import java.io.IOException;
-import java.lang.ref.Cleaner;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
 
 import application.Main;
 import gui.util.Alerts;
@@ -17,6 +16,7 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
+import model.services.DepartmentService;
 
 public class MainViewController implements Initializable{
 	
@@ -38,13 +38,16 @@ public class MainViewController implements Initializable{
 	@FXML
 	public void onMenuItemDepartmentAction() {
 		//System.out.println("onMenuItemSellerAction");
-		loadView("/gui/DepartmentList.fxml");
+		loadView("/gui/DepartmentList.fxml", (DepartmentListController controller) -> {
+			controller.setDepartmentService(new DepartmentService());  //ação de inicialização do controller departamento
+			controller.updeteTableView();
+		});
 	}
 
 	@FXML
 	public void onMenuItemAboutAction() {
 		//System.out.println("onMenuItemSellerAction");
-		loadView("/gui/About.fxml");
+		loadView("/gui/About.fxml", x -> {});
 	}
 
 
@@ -61,29 +64,29 @@ public class MainViewController implements Initializable{
 	 * ->porque eu vou passar o caminho completo
 	 * @param absoluteName
 	 */
-	private void loadView(String absoluteName) {
+	private synchronized <T>void loadView(String absoluteName, Consumer<T> initializengAction) {
 		try {
 			//carregar uma tela
 			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
 			VBox newVBox = loader.load();
 			
 			
-			Scene mainScene = Main.getMainScene(); //para que eu tenha condição de trabalhar com a janela principal eu vou ter que pegar uma referência da cena a cena em que a janela principal está ela foi declarada na classe main
-			
+			//para que eu tenha condição de trabalhar com a janela principal eu vou 
+			//ter que pegar uma referência da cena a cena em que a janela principal está ela foi declarada na classe main
+			Scene mainScene = Main.getMainScene();
 			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent(); //referencia do VBOX da janela principal
 			
 			Node mainMenu = mainVBox.getChildren().get(0);//guardar uma referência para o menu
-			
 			mainVBox.getChildren().clear(); //limpar todos os filhos limpar mainVBox
-			
 			mainVBox.getChildren().add(mainMenu);
 			mainVBox.getChildren().addAll(newVBox.getChildren());
 			
+			T controller = loader.getController();
+			initializengAction.accept(controller);
 			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			Alerts.showAlert("IO Exeception", "Error loading view", e.getMessage(), AlertType.ERROR);
 		}
 	}
-	
 }
---
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,240 @@
+package model.dao.impl;
+
+import db.DB;
+import db.DbException;
+import db.DbIntegrityException;
+import model.dao.SellerDao;
+import model.entities.Department;
+import model.entities.Seller;
+
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class SellerDaoJDBC implements SellerDao {
+
+    private Connection conn;
+
+    public SellerDaoJDBC(Connection conn){
+        this.conn = conn;
+    }
+
+    @Override
+    public void insert(Seller obj) {
+        PreparedStatement pst = null;
+        try {
+            pst = conn.prepareStatement(
+                    "INSERT INTO seller "
+                    + "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
+                    + "VALUES "
+                    + "(?, ?, ?, ?, ?)",
+                    Statement.RETURN_GENERATED_KEYS); //retornar o id do novo vendedor inserido.
+
+            pst.setString(1, obj.getName());
+            pst.setString(2, obj.getEmail());
+            pst.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+            pst.setDouble(4, obj.getBaseSalary());
+            pst.setInt(5, obj.getDepartment().getIdDepartment());
+
+            int rowsAffected = pst.executeUpdate();
+
+            if (rowsAffected > 0){
+                ResultSet rs = pst.getGeneratedKeys();
+                if (rs.next()){
+                    int id = rs.getInt(1);
+                    obj.setIdSeller(id);
+                }
+                DB.closeResultSet(rs);
+            }else {
+                throw new DbException("Unexpected error! No rows affected");
+            }
+
+        }
+        catch (SQLException e) {
+                throw new DbException(e.getMessage());
+        }
+        finally {
+            DB.closeStatement(pst);
+            //DB.closeConnection();
+        }
+    }
+
+    @Override
+    public void update(Seller obj) {
+        PreparedStatement pst = null;
+        try {
+            pst = conn.prepareStatement("UPDATE seller "
+                                        + "SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? "
+                                        + "WHERE Id = ?");
+
+            pst.setString(1, obj.getName());
+            pst.setString(2, obj.getEmail());
+            pst.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+            pst.setDouble(4, obj.getBaseSalary());
+            pst.setInt(5, obj.getDepartment().getIdDepartment());
+            pst.setInt(6, obj.getIdSeller());
+
+            pst.executeUpdate();
+        }
+        catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+        finally {
+            DB.closeStatement(pst);
+            //DB.closeConnection();
+        }
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+        PreparedStatement pst = null;
+        try{
+            pst = conn.prepareStatement("DELETE FROM seller WHERE Id = ?");
+
+            pst.setInt(1, id);
+
+            pst.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new DbIntegrityException(e.getMessage());
+        }
+        finally {
+            DB.closePreparedStatement(pst);
+        }
+    }
+
+    @Override
+    public Seller findById(Integer id) {
+        PreparedStatement pst = null;
+        ResultSet rs = null;
+        try {
+            pst = conn.prepareStatement("SELECT seller.*,department.Name as DepName\n"
+                                        + "FROM seller INNER JOIN department\n"
+                                        + "ON seller.DepartmentId = department.Id\n"
+                                        + "WHERE DepartmentId = ? ORDER BY Name");
+
+            pst.setInt(1,id);
+            rs = pst.executeQuery();
+
+            if (rs.next()){
+                Department dep = instantiateDepartment(rs);
+                Seller obj = instantiateSeller(rs,dep);
+                return obj;
+            }
+            return null;
+        }
+        catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+        finally {
+            DB.closePreparedStatement(pst);
+            DB.closeResultSet(rs);
+        }
+    }
+
+    private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException {
+        Seller obj = new Seller();
+        obj.setIdSeller(rs.getInt("Id"));
+        obj.setName(rs.getString("Name"));
+        obj.setEmail(rs.getString("Email"));
+        obj.setBaseSalary(rs.getDouble("BaseSalary"));
+        obj.setBirthDate(rs.getDate("BirthDate"));
+        obj.setDepartment(dep);
+        return obj;
+    }
+
+    private Department instantiateDepartment(ResultSet rs) throws SQLException {
+        Department dep = new Department();
+        dep.setIdDepartment(rs.getInt("DepartmentId"));
+        dep.setNameDepartment(rs.getString("DepName"));
+        return dep;
+    }
+
+    @Override
+    public List<Seller> findAll() {
+        PreparedStatement pst = null;
+        ResultSet rs = null;
+        try {
+            pst = conn.prepareStatement("SELECT seller.*,department.Name as DepName "
+                                        + "FROM seller INNER JOIN department "
+                                        + "ON seller.DepartmentId = department.Id "
+                                        + "ORDER BY Name");
+
+            rs = pst.executeQuery();
+
+            List<Seller> list = new ArrayList<>();
+            Map<Integer, Department> map = new HashMap<>();
+
+            while (rs.next()) {
+
+                Department dep = map.get(rs.getInt("DepartmentId"));
+
+                if (dep == null) {
+                    dep = instantiateDepartment(rs);
+                    map.put(rs.getInt("DepartmentId"), dep);
+                }
+
+                Seller obj = instantiateSeller(rs, dep);
+                list.add(obj);
+            }
+            return list;
+        }
+        catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+        finally {
+            DB.closeStatement(pst);
+            DB.closeResultSet(rs);
+        }
+    }
+
+    @Override
+    public List<Seller> findByDepartment(Department department) {
+        PreparedStatement pst = null;
+        ResultSet rs = null;
+        try {
+            pst = conn.prepareStatement("SELECT seller.*,department.Name as DepName "
+                                        + "FROM seller INNER JOIN department "
+                                        + "ON seller.DepartmentId = department.Id "
+                                        + "WHERE DepartmentId = ? "
+                                        + "ORDER BY Name");
+
+            pst.setInt(1, department.getIdDepartment());
+
+            rs = pst.executeQuery();
+
+            List<Seller> list = new ArrayList<>();
+            Map<Integer, Department> map = new HashMap<>();
+
+            while (rs.next()) {
+
+                Department dep = map.get(rs.getInt("DepartmentId"));
+
+                if (dep == null) {
+                    dep = instantiateDepartment(rs);
+                    map.put(rs.getInt("DepartmentId"), dep);
+                }
+
+                Seller obj = instantiateSeller(rs, dep);
+                list.add(obj);
+            }
+            return list;
+        }
+        catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+        finally {
+            DB.closeStatement(pst);
+            DB.closeResultSet(rs);
+        }
+    }
+
+
+}
