287c93c7717a8ea02b476038bf2896c701503a91
murillosm <murillosm@hotmail.com>
2022-11-11 20:50:37 -0300

    DepartmentForm (dialog) design

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,18 +1,27 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import gui.util.Alerts;
+import gui.util.Utils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Department;
 import model.services.DepartmentService;
@@ -36,8 +45,9 @@ public class DepartmentListController implements Initializable{
 	private ObservableList<Department> obsList;
 	
 	@FXML
-	public void onBtNewAction() {
-		System.out.println("onBtNewAction");
+	public void onBtNewAction(ActionEvent event) {
+		Stage parantStage = Utils.currentStage(event);
+		createDialogForm("/gui/DepartmentForm.fxml", parantStage);
 	}
 	
 	public void setDepartmentService(DepartmentService service) {
@@ -69,6 +79,26 @@ public class DepartmentListController implements Initializable{
 		obsList = FXCollections.observableArrayList(list);//instancia o ObservableList pegando os dados originais da lista
 		
 		tableViewDepartment.setItems(obsList);//carregar os itens na tableView e mostrar na tela
+	}
+	
+	private void createDialogForm(String absoluteName, Stage parentStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));  //Abrir janela de formulario
+			Pane pane = loader.load(); //carregar view 
+			
+			/**quando carregar uma janela de diálogo modal na frente 
+			da janela existente e necessario que instancia um novo stage*/
+			Stage dialogStage = new Stage();
+			dialogStage.setTitle("Enter Department data"); //configurar o título da janela
+			dialogStage.setScene(new Scene(pane));//nova cena
+			dialogStage.setResizable(false); //diz se janela pode ou não ser redimencionada
+			dialogStage.initOwner(parentStage);
+			dialogStage.initModality(Modality.WINDOW_MODAL);//a janela vai ficar travada enquanto você não fechar não pode acessar a janela anterior
+			dialogStage.showAndWait();//carregar a janela do formulário
+			
+		}catch (IOException e) {
+			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
 	}
 
 }
---
+++ src/gui/util/Utils.java
@@ -0,0 +1,13 @@
+package gui.util;
+
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+
+public class Utils {
+	//ActionEvent por exemplo o botão recebeu um click
+	public static Stage currentStage(ActionEvent event) { 
+		//acessar o Stage onde o meu controle que recebeu o evento está por exemplo o seu clique no botão
+		return (Stage) ((Node) event.getSource()).getScene().getWindow(); 	}
+
+}
