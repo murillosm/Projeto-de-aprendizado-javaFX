52dfd5596da58d6b635daca057d357a8e5e357da
murillosm <murillosm@hotmail.com>
2022-11-12 21:37:45 -0300

    Remove department

--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -1,6 +1,5 @@
 package model.services;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -8,18 +7,23 @@ import model.dao.DepartmentDao;
 import model.entities.Department;
 
 public class DepartmentService {
-	
+
 	private DepartmentDao dao = DaoFactory.createDepartmentDao();
-	
+
+
 	public List<Department> findAll() {
 		return dao.findAll();
 	}
-	
+
 	public void saveOrUpdate(Department obj) {
 		if (obj.getIdDepartment() == null) {
 			dao.insert(obj);
-		}else {
+		} else {
 			dao.update(obj);
 		}
+	}
+
+	public void remove(Department obj) {
+		dao.deleteById(obj.getIdDepartment());
 	}
 }
